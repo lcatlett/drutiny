bfcab1f9c43e5adf5785c45b62aa4d0bc51ae7ef
Josh Waihi <josh.waihi@acquia.com>
2022-05-24 09:09:12 +1200

    Added rendering format for dependency report.

--- src/Report/Format/Terminal.php
+++ src/Report/Format/Terminal.php
@@ -13,18 +13,35 @@ class Terminal extends Markdown
 {
     protected string $name = 'terminal';
 
+    /**
+     * Change the report template to use a profile dependency report format.
+     */
+    public function setDependencyReport()
+    {
+      $this->options['content'] = $this->loadTwigTemplate('report/dependency');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function render(Profile $profile, AssessmentInterface $assessment):FormatInterface
     {
         parent::render($profile, $assessment);
-        $this->buffer->write(self::format($this->buffer->fetch()));
+        $this->buffer->write(Renderer::createFromMarkdown($this->buffer->fetch()));
         return $this;
     }
 
-    public static function format(string $output)
+    /**
+     * @deprecated
+     */
+    protected static function format(string $output)
     {
         return Renderer::createFromMarkdown($output);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function write():iterable
     {
         $this->container->get('output')->write($this->buffer->fetch());
