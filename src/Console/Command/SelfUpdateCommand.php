cdf5371b490126d5b234817250c3947b57cd88db
Josh Waihi <josh.waihi@acquia.com>
2023-02-07 11:37:07 +1300

    Update self-upgrade to use extraction method.

--- src/Console/Command/SelfUpdateCommand.php
+++ src/Console/Command/SelfUpdateCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
 
 /**
  * Self update command.
@@ -34,20 +35,9 @@ class SelfUpdateCommand extends DrutinyBaseCommand
         $io = new SymfonyStyle($input, $output);
         $logger = $this->getContainer()->get('logger');
 
-        if (!\Phar::running()) {
-            $io->error("This is not a self-upgradable release. Please use the latest Phar release file.");
-            return 2;
-        }
-
         $current_version = $this->getApplication()->getVersion();
 
         $composer_json = json_decode(file_get_contents(DRUTINY_LIB . '/composer.json'), true);
-
-        $current_script = realpath($_SERVER['SCRIPT_NAME']);
-        if (!is_writable($current_script)) {
-            $io->error("Cannot write to $current_script. Will not be able to apply update.");
-            return 3;
-        }
 
         $headers = [
           'User-Agent' => $this->getApplication()->getName() . ' drutiny-phar/' . $current_version,
@@ -92,7 +82,7 @@ class SelfUpdateCommand extends DrutinyBaseCommand
 
         $tmpfile = tempnam(sys_get_temp_dir(), $download['name']);
         $resource = fopen($tmpfile, 'w');
-        // $file_path = fopen(realpath($_SERVER['SCRIPT_NAME']),'w');
+
         $logger->notice("Downloading {$download['name']}...");
 
         $response = $client->get('repos/' . $composer_json['name'] . '/releases/assets/' . $download['id'], [
@@ -106,19 +96,10 @@ class SelfUpdateCommand extends DrutinyBaseCommand
 
         chmod($tmpfile, 0766);
 
-        $logger->notice("New release downloaded to $tmpfile.");
-
-        // Avoid errors shutting down the current process my replacing the phar
-        // file on shutdown.
-        register_shutdown_function(function () use ($tmpfile, $current_script) {
-            if (!rename($tmpfile, $current_script)) {
-                echo "ERROR: Could not overwrite $current_script with $tmpfile.\n";
-                return 1;
-            }
-        });
-
-        $io->newLine();
-        $io->success("Updated to $new_version.");
-        return 0;
+        $process = new Process([$tmpfile, '--version']);
+        $logger->notice("{$download['name']} downloaded to $tmpfile.");
+        $status = $process->setTty(true)->setPty(true)->run();
+        unlink($tmpfile);
+        return $status;
     }
 }
