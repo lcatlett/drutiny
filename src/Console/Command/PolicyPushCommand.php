5debc00233b1de8c097777b08d12ccced5d444c3
Josh Waihi <josh.waihi@acquia.com>
2023-05-21 08:40:02 +1200

    Made policy source selection easier in policy:push command.

--- src/Console/Command/PolicyPushCommand.php
+++ src/Console/Command/PolicyPushCommand.php
@@ -2,7 +2,9 @@
 
 namespace Drutiny\Console\Command;
 
+use Drutiny\Attribute\AsSource;
 use Drutiny\PolicyFactory;
+use Drutiny\PolicySource\PolicySourceInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -11,6 +13,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Drutiny\PolicySource\PushablePolicySourceInterface;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 
 /**
  *
@@ -41,7 +44,7 @@ class PolicyPushCommand extends DrutinyBaseCommand
         )
         ->addArgument(
             'source',
-            InputArgument::REQUIRED,
+            InputArgument::OPTIONAL,
             'The name of the source to push too.'
         );
     }
@@ -51,7 +54,7 @@ class PolicyPushCommand extends DrutinyBaseCommand
    */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $source = $this->policyFactory->getSource($input->getArgument('source'));
+        $source = $this->getPushSource($input, $output);
 
         $io = new SymfonyStyle($input, $output);
 
@@ -78,5 +81,31 @@ class PolicyPushCommand extends DrutinyBaseCommand
         ));
 
         return 0;
+    }
+
+    /**
+     * Find an appropriate source to push too.
+     */
+    protected function getPushSource(InputInterface $input, OutputInterface $output):PushablePolicySourceInterface
+    {
+      $io = new SymfonyStyle($input, $output);
+      if ($source_name = $input->getArgument('source')) {
+        return $this->policyFactory->getSource($input->getArgument('source'));
+      }
+      $sources = array_filter($this->policyFactory->sources, function (AsSource $source) {
+        return $this->policyFactory->getSource($source->name) instanceof PushablePolicySourceInterface;
+      });
+      if (count($sources) == 1) {
+        $name = array_shift($sources)->name;
+        if ($io->confirm("Push policy to source '$name'?")) {
+          return $this->policyFactory->getSource($name);
+        }
+        throw new InvalidArgumentException("There are no pushable sources to push policies too.");
+      }
+      if (count($sources) == 0) {
+        throw new InvalidArgumentException("There are no pushable sources to push policies too.");
+      }
+      $choice = $io->choice("Which source would you like to push to?", array_keys($sources));
+      return $this->policyFactory->getSource($sources[$choice]);
     }
 }
