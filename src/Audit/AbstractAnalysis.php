2e4c2013ceb076cd0b8a2cff5f1c38a16d56f4e7
Josh Waihi <josh.waihi@acquia.com>
2023-05-24 13:03:17 +1200

    Added warnIf and failIf parameters to AbstractAnalysis class.

--- src/Audit/AbstractAnalysis.php
+++ src/Audit/AbstractAnalysis.php
@@ -2,55 +2,25 @@
 
 namespace Drutiny\Audit;
 
+use Drutiny\Attribute\Parameter;
+use Drutiny\Attribute\Type;
 use Drutiny\Audit;
+use Drutiny\AuditResponse\State;
 use Drutiny\Sandbox\Sandbox;
 use Twig\Error\RuntimeError;
 
 /**
  * Audit gathered data.
  */
+#[Parameter(name: 'expression', type: Type::STRING, description: 'Twig syntax to evaluate audit data into an outcome.', default: 'true')]
+#[Parameter(name: 'variables', type: Type::HASH, description: 'A keyed array of expressions to set variables before evaluating the passing expression.', default: [])]
+#[Parameter(name: 'syntax', type: Type::STRING, enums: ['twig', 'expression_language'], default: 'expression_language', description: 'Which syntax to use. Options: twig or expression_language.')]
+#[Parameter(name: 'not_applicable', type: Type::STRING, default: 'false', description: 'An expression that if returns true, will render the policy as not applicable.')]
+#[Parameter(name: 'fail_on_error', type: Type::BOOLEAN, default: false, description: 'Set to true if you want an error during data gathering to result in a failure.')]
+#[Parameter(name: 'failIf', type: Type::STRING, description: 'Fail policy if twig expression returns true')]
+#[Parameter(name: 'warningIf', type: Type::STRING, description: 'Add warning to outcome if expression return true')]
 class AbstractAnalysis extends Audit
 {
-  public function configure(): void
-  {
-    $this->addParameter(
-        'expression',
-        static::PARAMETER_OPTIONAL,
-        'The expression language to evaluate. See https://symfony.com/doc/current/components/expression_language/syntax.html',
-        'true',
-      )
-      ->addParameter(
-        'warning',
-        static::PARAMETER_OPTIONAL,
-        'The expression language to evaludate if the analysis is not applicable. See https://symfony.com/doc/current/components/expression_language/syntax.html',
-        'false'
-      )
-      ->addParameter(
-        'variables',
-        static::PARAMETER_OPTIONAL,
-        'A keyed array of expressions to set variables before evaluating the passing expression.',
-        []
-      )
-      ->addParameter(
-        'syntax',
-        static::PARAMETER_OPTIONAL,
-        'expression_language or twig',
-        'expression_language'
-      )
-      ->addParameter(
-        'not_applicable',
-        static::PARAMETER_OPTIONAL,
-        'The expression language to evaludate if the analysis is not applicable. See https://symfony.com/doc/current/components/expression_language/syntax.html',
-        'false'
-      )
-      ->addParameter(
-        'fail_on_error',
-        static::PARAMETER_OPTIONAL,
-        'Set to true if you want an error during data gathering to result in a failure.',
-        false
-      );
-  }
-
   /**
    * Gather analysis data to audit.
    */
@@ -83,18 +53,36 @@ class AbstractAnalysis extends Audit
         foreach ($this->getParameter('variables',[]) as $key => $value) {
           try {
             $this->logger->debug(__CLASS__ . ':VARIABLE('.$key.'): ' . $value);
-            $this->set($key, $this->evaluate($value, $syntax));
+            $this->set($key, $this->evaluate($value, 'twig'));
           }
           catch (RuntimeError $e)
           {
-            throw new \Exception("Failed to create key: $key. Encountered Twig runtime error: " . $e->getMessage());
+            throw new \Exception("Failed to create variable: $key. Encountered Twig runtime error: " . $e->getMessage());
           }
         }
 
-        $expression = $this->getParameter('expression', 'true');
-        $this->logger->debug(__CLASS__ . ':EXPRESSION: ' . $expression);
-        $output = $this->evaluate($expression, $syntax);
-        $this->logger->debug(__CLASS__ . ':EVALUATION: ' . json_encode($output));
-        return $output;
+        // Set outcome to failure if failIf expression is true.
+        // Note: This takes precedence of the `expression` parameter which does not get evaluated
+        // if this parameter is set.
+        $failIf = $this->getParameter('failIf');
+        if ($failIf !== null) {
+          $this->logger->debug(__CLASS__ . ':EXPRESSION(failIf): ' . $failIf);
+          $outcome = $this->evaluate($failIf, 'twig') ? State::FAILURE : State::SUCCESS;
+        }
+        else {
+          $expression = $this->getParameter('expression', 'true');
+          $this->logger->debug(__CLASS__ . ':EXPRESSION: ' . $expression);
+          $outcome = State::fromValue($this->evaluate($expression, $syntax));
+        }
+
+        // Add a warning if warningIf expression is true.
+        if (($outcome->isSuccessful() || $outcome->isFailure()) && !$outcome->hasWarning()) {
+          $warningIf = $this->getParameter('warningIf');
+          if ($warningIf !== null) {
+            $this->logger->debug(__CLASS__ . ':EXPRESSION(warningIf): ' . $warningIf);
+            $outcome = $this->evaluate($warningIf, 'twig') ? $outcome->withWarning() : $outcome;
+          }
+        }
+        return $outcome->value;
     }
 }
