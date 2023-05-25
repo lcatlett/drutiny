77d8b8e9af128e3129fff24de2ff6cc02efa8903
Josh Waihi <josh.waihi@acquia.com>
2023-05-25 13:19:11 +1200

    Upgrade audits to work with Parameter attributes.

--- src/Audit/Drupal/PhpIniAnalysis.php
+++ src/Audit/Drupal/PhpIniAnalysis.php
@@ -3,52 +3,13 @@
 namespace Drutiny\Audit\Drupal;
 
 use Drutiny\Audit\AbstractAnalysis;
-use Drutiny\Sandbox\Sandbox;
-use Drutiny\Annotation\Param;
-
 
 class PhpIniAnalysis extends AbstractAnalysis
 {
-
-  public function configure():void
-  {
-    $this->addParameter(
-        'expression',
-        static::PARAMETER_REQUIRED,
-        'The expression language to evaluate. See https://symfony.com/doc/current/components/expression_language/syntax.html'
-      )
-      ->addParameter(
-        'warning',
-        static::PARAMETER_OPTIONAL,
-        'The expression language to evaludate if the analysis is not applicable. See https://symfony.com/doc/current/components/expression_language/syntax.html',
-        'false'
-      )
-      ->addParameter(
-        'variables',
-        static::PARAMETER_OPTIONAL,
-        'A keyed array of expressions to set variables before evaluating the passing expression.',
-        []
-      )
-      ->addParameter(
-        'syntax',
-        static::PARAMETER_OPTIONAL,
-        'expression_language or twig',
-        'expression_language'
-      )
-      ->addParameter(
-        'not_applicable',
-        static::PARAMETER_OPTIONAL,
-        'The expression language to evaludate if the analysis is not applicable. See https://symfony.com/doc/current/components/expression_language/syntax.html',
-        'false'
-      );
-  }
-
   /**
    * @inheritdoc
    */
-  public function gather(Sandbox $sandbox) {
-    parent::gather($sandbox);
-
+  public function gather() {
     $phpini = $this->target->getService('drush')->runtime(function () {
         return ini_get_all();
     });
