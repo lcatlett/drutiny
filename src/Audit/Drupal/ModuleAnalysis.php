ad6519613e9cc9e5267262c38cd93319c9173d89
Josh Waihi <josh.waihi@acquia.com>
2023-06-12 11:22:27 +1200

    Syntax updates to module analysis.

--- src/Audit/Drupal/ModuleAnalysis.php
+++ src/Audit/Drupal/ModuleAnalysis.php
@@ -2,20 +2,21 @@
 
 namespace Drutiny\Audit\Drupal;
 
+use Drutiny\Attribute\DataProvider;
 use Drutiny\Audit\AbstractAnalysis;
 use Drutiny\Helper\TextCleaner;
+use Drutiny\Policy\Dependency;
 use Drutiny\Sandbox\Sandbox;
 
 /**
  * Generic module is enabled check.
  *
  */
+#[Dependency('Drupal.isBootstrapped')]
 class ModuleAnalysis extends AbstractAnalysis
 {
-  /**
-   *
-   */
-    public function gather(Sandbox $sandbox)
+    #[DataProvider]
+    public function listModules():void
     {
         $list = $this->target->getService('drush')
           ->pmList([
