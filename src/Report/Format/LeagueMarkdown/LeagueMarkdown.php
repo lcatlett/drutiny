f5654e6846047c713bdab86dfa962078f7a48783
Josh Waihi <josh.waihi@acquia.com>
2023-06-27 17:00:54 +1200

    Added backwards compatible markdown formatters.

--- src/Report/Format/LeagueMarkdown/LeagueMarkdown.php
+++ src/Report/Format/LeagueMarkdown/LeagueMarkdown.php
@@ -2,8 +2,10 @@
 
 namespace Drutiny\Report\Format\LeagueMarkdown;
 
+use Drutiny\Report\Format\Markdown;
 use League\CommonMark\ConverterInterface;
 use League\CommonMark\Extension\Table\TableCell;
+use League\CommonMark\Extension\Table\TableRow;
 use League\CommonMark\Extension\Table\TableSection;
 use Twig\Extra\Markdown\MarkdownInterface;
 
@@ -13,9 +15,16 @@ class LeagueMarkdown implements MarkdownInterface
 
     public function convert(string $body): string
     {
+        // Reformat to support old Parsedown syntax.
+        $body = Markdown::formatTables($body);
+        $body = Markdown::formatHeadings($body);
+
         return $this->converter->convert($body);
     }
 
+    /**
+     * As the col scope to th elements.
+     */
     public static function tableCellScopeAttribute(TableCell $node): ?string
     {
         $tag = $node->getType() === TableCell::TYPE_HEADER ? 'th' : 'td';
@@ -29,6 +38,20 @@ class LeagueMarkdown implements MarkdownInterface
         $tag = $node->getType() === TableSection::TYPE_HEAD ? 'thead' : 'tbody';
 
         if ($tag == 'thead') {
+            return 'table-active';
+        }
+        return null;
+    }
+
+    /**
+     * Adds the table-active class to tr elements inside a thead.
+     */
+    public static function tableRowClassAttribute(TableRow $node): ?string
+    {
+        if (!($node->parent() instanceof TableSection)) {
+            return null;
+        }
+        if ($node->parent()->getType() == TableSection::TYPE_HEAD) {
             return 'table-active';
         }
         return null;
--- src/Report/Format/Markdown.php
+++ src/Report/Format/Markdown.php
@@ -40,48 +40,100 @@ class Markdown extends HTML
       return $sections;
     }
 
-    public static function formatTables($markdown)
+    public static function formatHeadings(string $markdown): string {
+
+        $lines = explode(PHP_EOL, $markdown);
+
+        foreach ($lines as $idx => $line) {
+            if (strpos($line, '#') === false) {
+                continue;
+            }
+
+            // Convert headings like '##Heading' to '## Heading'.
+            // This is to support the shift from Parsedown to Commonmark.
+            $updated_line = strpos($line, '#') !== false ? preg_replace('/^((?:\s*)?[\#]+)([^ \#]+.*)$/', '$1 $2', $line) : $line;
+            if ($updated_line != $line) {
+                $lines[$idx] = $updated_line;
+            }
+        }
+        return implode(PHP_EOL, $lines);
+    }
+
+    public static function formatTables(string $markdown): string
     {
         $lines = explode(PHP_EOL, $markdown);
         $table = [
-        'start' => null,
-        'widths' => [],
-        'rows' => [],
+            'start' => null,
+            'widths' => [],
+            'rows' => [],
+            'cols' => 0,
+            'leading_pipes' => false
         ];
 
         foreach ($lines as $idx => $line) {
+            // Start table
             if ($table['start'] === null) {
                 if (strpos($line, ' | ') !== false) {
                     $table['start'] = $idx;
                 } else {
                     continue;
                 }
-            } elseif (strpos($line, ' | ') === false) {
-                foreach ($table['rows'] as $line_idx => $row) {
-                    $widths = $table['widths'];
+            }
+            // The line underneath the table headers
+            elseif (($table['start'] + 1) == $idx) {
+                $table['rows'][$idx] = [];
+                continue;
+            } 
+            // End table
+            elseif (strpos($line, ' | ') === false) {
+                // Only process if there were data cells to process. Just a header isn't a table.
+                if (count($table['rows']) > 2) {
+                    foreach ($table['rows'] as $line_idx => $row) {
+                        $table_line = array_search($line_idx, array_keys($table['rows']));
 
-                    foreach ($row as $i => $value) {
-                        $pad = array_search($line_idx, array_keys($table['rows'])) === 1 ? '-' : ' ';
-                        $row[$i] = str_pad($value, $table['widths'][$i], $pad, STR_PAD_RIGHT);
+                        // Look for the header seperator line and use dash '-' instead of space pads.
+                        $pad = $table_line === 1 ? '-' : ' ';
+                        $row = array_pad($row, $table['cols'], $pad);
+                        foreach ($row as $i => $value) {    
+                            $row[$i] = str_pad($value, $table['widths'][$i], $pad, STR_PAD_RIGHT);
+                        }
+
+                        // For the header seperator line, we don't add pads to the first "column"
+                        // on tables with leading pipes.
+                        if ($pad == '-' && $table['leading_pipes']) {
+                            $row[0] = '';
+                        }
+
+                        $lines[$line_idx] = implode(' | ', $row);
                     }
-                    $lines[$line_idx] = implode(' | ', $row);
                 }
 
-                $table['start']  = null;
-                $table['widths'] = [];
-                $table['rows']   = [];
+                $table = [
+                    'start' => null,
+                    'widths' => [],
+                    'rows' => [],
+                    'cols' => 0,
+                    'leading_pipes' => false
+                ];
                 continue;
+            }
+
+            // This is a table line.
+            // Does the table line start with a |?
+            if (strpos(trim($line), '|') === 0) {
+                $table['leading_pipes'] = true;
             }
 
             $cells = array_map('trim', explode('|', $line));
 
+            // Set the max width for each column.
             foreach ($cells as $i => $value) {
-                if (!isset($table['widths'][$i])) {
-                    $table['widths'][$i] = strlen($value);
-                } else {
-                    $table['widths'][$i] = max($table['widths'][$i], strlen($value));
-                }
+                $table['widths'][$i] = max($table['widths'][$i] ?? 0, strlen($value));
             }
+
+            $table['cols'] = max(count($cells), $table['cols']);
+
+            // Collect the cells.
             $table['rows'][$idx] = $cells;
         }
 
