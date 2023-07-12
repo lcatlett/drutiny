6373f8b257de66f971dfbc96af9ddbf73df8cc94
Josh Waihi <josh.waihi@acquia.com>
2023-07-12 12:34:38 -0400

    Improve how templates can access sections.

--- src/Report/Format/HTML.php
+++ src/Report/Format/HTML.php
@@ -18,7 +18,7 @@ class HTML extends TwigFormat
             $definition = $definition->with(content: $this->loadTwigTemplate('report/profile'));
         }
         elseif (is_string($definition->content)) {
-            $definition = $definition->with(content: $this->twig->createTemplate($definition->content));
+            $definition = $definition->with(content: $this->twig->createTemplate($definition->content, $this->getName()));
         }
         if (empty($definition->template)) {
           $definition = $definition->with(template: 'report/page.' . $this->getExtension() . '.twig');
@@ -32,8 +32,9 @@ class HTML extends TwigFormat
 
       // In 3.x we support Twig TemplateWrappers to be passed directly
       // to the report format.
+
       foreach ($this->definition->content->getBlockNames() as $block){
-        $sections[] = $this->definition->content->renderBlock($block, $variables);
+        $sections[$block] = $this->definition->content->renderBlock($block, $variables);
       }
       return $sections;
     }
