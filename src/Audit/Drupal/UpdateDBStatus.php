21c7e1fa9847d19efb715f614c878f5d6b3051c3
Josh Waihi <josh.waihi@acquia.com>
2023-10-31 06:46:39 +1300

    Revised audit class for updatedb status.

--- src/Audit/Drupal/UpdateDBStatus.php
+++ src/Audit/Drupal/UpdateDBStatus.php
@@ -2,43 +2,23 @@
 
 namespace Drutiny\Audit\Drupal;
 
-use Drutiny\Audit;
-use Drutiny\Sandbox\Sandbox;
+use Drutiny\Attribute\DataProvider;
+use Drutiny\Attribute\Version;
+use Drutiny\Audit\AbstractAnalysis;
+use Drutiny\Helper\TextCleaner;
 
 /**
  * Ensure all module updates have been applied.
  */
-class UpdateDBStatus extends Audit
+#[Version('2.0')]
+class UpdateDBStatus extends AbstractAnalysis
 {
-
-    /**
-     * A string which presence indicates updates are pending.
-     */
-    const AFFIRMATIVE_UPDATES_STRING = "Do you wish to run all pending updates";
-
-    /**
-     * @inheritdoc
-     */
-    public function audit(Sandbox $sandbox)
+    #[DataProvider]
+    public function getUpdateStatus()
     {
-        $output = $this->target->getService('drush')->updb('-n')->run();
-
-        if (strpos($output, self::AFFIRMATIVE_UPDATES_STRING) !== false) {
-            $lines = array_filter(explode(PHP_EOL, $output));
-            $updates = [];
-            while (strpos(current($lines), self::AFFIRMATIVE_UPDATES_STRING) === false) {
-                preg_match("/\s*([\w\s]*\w)\s+(\d+)\s+(.*)/", current($lines), $matches);
-                list(, $module, $revision, $message) = $matches;
-                $updates[] = [
-                'module' => $module,
-                'revision' => $revision,
-                'message' => $message
-                ];
-                next($lines);
-            }
-            $this->set('updates', $updates);
-            return false;
-        }
-        return true;
+        $updates = $this->target->getService('drush')->updatedbStatus(['format' => 'json'])->run(function ($output) {
+            return TextCleaner::decodeDirtyJson($output);
+        });
+        $this->set('updates', $updates);
     }
 }
