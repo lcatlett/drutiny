689c8865c0bfb62dd0060737d62b871801f78830
Josh Waihi <josh.waihi@acquia.com>
2023-11-15 23:05:27 +1300

    Decorated language trait with autoload attribute.

--- src/Console/Command/LanguageCommandTrait.php
+++ src/Console/Command/LanguageCommandTrait.php
@@ -2,6 +2,7 @@
 
 namespace Drutiny\Console\Command;
 
+use Drutiny\Attribute\Autoload;
 use Drutiny\LanguageManager;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,10 +12,12 @@ use Symfony\Component\Console\Input\InputInterface;
  */
 trait LanguageCommandTrait
 {
+    #[Autoload(early: true)]
     protected LanguageManager $languageManager;
-  /**
-   * @inheritdoc
-   */
+    
+    /**
+     * @inheritdoc
+     */
     protected function configureLanguage()
     {
         $this
