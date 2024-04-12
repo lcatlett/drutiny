0b740db52068b4a748dfb7fc0bc82eb418f7d096
Josh Waihi <josh.waihi@acquia.com>
2024-04-12 21:30:00 +1200

    Cleaning up TSH command output from responses.

--- src/Target/Transport/TshTransport.php
+++ src/Target/Transport/TshTransport.php
@@ -2,6 +2,7 @@
 
 namespace Drutiny\Target\Transport;
 
+use Psr\Cache\CacheItemInterface;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
@@ -20,7 +21,7 @@ class TshTransport extends SshTransport {
   public function send(Process $command, ?callable $processor = null)
   {
     try {
-      return parent::send($command, $processor);
+      return $this->sendAndProcess($command, $processor);
     }
     // Look for teleport issue where an ambiguous host was detected.
     catch (ProcessFailedException $e) {
@@ -44,8 +45,51 @@ class TshTransport extends SshTransport {
       $this->sshConfig['Host'] = $node;
 
       // Retry.
-      return parent::send($command, $processor);
+      return $this->sendAndProcess($command, $processor);
     }
+  }
+
+  /**
+   * Send and process.
+   *
+   * TSH can return command characters that mess up output
+   * so we will remove it here.
+   */
+  private function sendAndProcess(Process $command, ?callable $processor = null) {
+
+    if (is_callable($processor)) {
+      $reflect = new \ReflectionFunction($processor);
+      $params = $reflect->getParameters();
+
+      if (!empty($params) && ($params[0]->getType() == Process::class)) {
+        return parent::send($command, function (Process $process, CacheItemInterface $item) use ($processor) {
+          return self::preprocess($process, $item, $processor);
+        });
+      }
+    }
+    return parent::send($command, function (string $output, CacheItemInterface $item) use ($processor) {
+      return self::preprocess($output, $item, $processor);
+    });
+  }
+
+  /**
+   * Preprocess output from a TSH call to remove command characters.
+   */
+  public static function preprocess(Process|string $process, CacheItemInterface $item, ?callable $processor = null) {
+    $output = is_string($process) ? $process : $process->getOutput();
+    // @see https://unix.stackexchange.com/questions/14684/removing-control-chars-including-console-codes-colours-from-script-output/14707#14707
+    $output = preg_replace('/ \e[ #%()*+\-.\/]. | \e\[ [ -?]* [@-~] | \e\] .*? (?:\e\\|[\a\x9c]) | \e[P^_] .*? (?:\e\\|\x9c) | \e. /x', '', $output);
+    if (is_callable($processor) && $process instanceof Process) {
+      $reflect = new \ReflectionFunction($processor);
+      $params = $reflect->getParameters();
+
+      if (!empty($params) && ($params[0]->getType() == Process::class)) {
+        $process->clearOutput();
+        $process->addOutput($output);
+        return $processor($process, $item);
+      }
+    }
+    return $processor ? $processor($output, $item) : $output;
   }
 
   /**
